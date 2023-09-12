7f970768842125026dc092aa8a48825745376ffc
Isabella Costa <isa_bella411@hotmail.com>
2023-09-12 11:39:59 -0300

    mudando localização e formatação de numeros e datas

--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -1,16 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-
+using System.ComponentModel.DataAnnotations;
 namespace SalesWebMvc.Models
 {
     public class Seller
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
+        [Display(Name="Birth Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString ="{0:dd/MM/yyyy}")]
         public DateTime  BirthDate{ get; set; }
-         public double BaseSalary { get; set; }
+        [Display(Name = "Base Salary")]
+        [DisplayFormat(DataFormatString ="{0:f2}")]
+        public double BaseSalary { get; set; }
         public Department Department { get; set; }
         public int DepartmentId { get; set; }
         ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
